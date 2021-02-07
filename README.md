c95636f259715bf6d9beba82ec027044ec88f649
dean <wenbindu@yeah.net>
2021-02-07 15:05:44 +0800

    init tool repository and add yyets donwload helper.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # HungryTools
-some tools to make life simple.
+> some tools to make life simple.
+
+## Create Python Env
+`python3 -m venv venv`
+
+## Install Dependency
+get into the venv and install independency.
+- `source venv/bin/activate`
+- `pip install -r requirements.txt`
+
+## Tools
+1. yyets download helper.
+2. 
+
+
+[1] yyets downloader
+
+> help download yyets files from commandline.
+
+usage: 
+```python
+python main.py
+# then input the id of the resource.
+# input the index with hits.
+```
+
---
+++ yyets-crawler/main.py
@@ -0,0 +1,70 @@
+import requests
+
+
+def get_api_result(_id):
+    """get json from api"""
+    url = f"https://yyets.dmesg.app/api/resource?id={_id}"
+    headers = {
+        "referer": f"https://yyets.dmesg.app/resource.html?id={_id}",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+    }
+    resp = requests.get(url, timeout=10, headers=headers).json()
+    # print(resp)
+    if not resp['info'] == 'OK':
+        print("some error!")
+        return
+    info = resp['data']['info']
+    print(f"To Download: {info['cnname']}")
+    resource_list = resp['data']['list']
+    indexs = {r['season_num']: r['season_cn'] for r in resource_list}
+    items = {r['season_num']: r['items'] for r in resource_list}
+    print("*"*20)
+    for k,v in indexs.items():
+        print(f"[{k}]{v}") 
+    print("*" * 20)
+    num = None
+    while True:
+        num = input("Please Input Index[*]:")
+        num = num.strip()
+        if num in indexs.keys():
+            break
+    print(f"Selected Num: {indexs.get(num)}")
+    item = items.get(num)
+    print("*" * 20)
+    for i, v in enumerate(item.keys()):
+        print(f"[{i}]{v}")
+    format_num = None
+    while True:
+        format_num = input("Please Input Format Index[*]:")
+        format_num = format_num.strip()
+        if format_num in [str(i) for i in range(len(item.keys()))]:
+            break
+    
+    format_name = list(item.keys())[int(format_num)]
+    print(f"Selected Format: {format_name}")
+    item_list = item.get(format_name)
+    magenets = []
+    for f in item_list:
+        files = f['files']
+        for m in files:
+            if m['way_cn'] == "磁力":
+                magenets.append(m['address'])
+    
+    print("*** All Magnet Urls ***")
+    for k in magenets:
+        print(k)
+    print("*** Done ***")
+
+
+def crawler(url):
+    """"crawler of the https://yyets.dmesg.app/ """
+    print(f"source url: {url}")
+    _, r_id = url.split("=")
+    print(f"resource id: {r_id}")
+    get_api_result(r_id)
+
+
+if __name__=="__main__":
+    resource = input("Please Input Id: ")
+    # https://yyets.dmesg.app/resource.html?id=36232
+    get_api_result(resource.strip())
